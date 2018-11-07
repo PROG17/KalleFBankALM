c805e3704521b3b163736887439f4818e7266562
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2018-11-07 15:51:00 +0100

    Feature#1: Changed startup page to Listcustomers

--- KalleFBankALM/Startup.cs
+++ KalleFBankALM/Startup.cs
@@ -56,7 +56,7 @@ namespace KalleFBankALM
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=ListCustomers}/{id?}");
             });
         }
     }
