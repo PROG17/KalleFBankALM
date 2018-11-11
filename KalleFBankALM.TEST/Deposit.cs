f0f93a14ac838a4767611d0a1eac9174d8ee6951
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2018-11-11 17:20:58 +0100

    Feature2: lagt till deposittest

---
+++ KalleFBankALM.TEST/Deposit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace KalleFBankALM.TEST
+{
+    public class Deposit
+    {
+        private decimal _testAmountCorrect = 40m;
+        private decimal _testBalance = 80m;
+
+        [Fact]
+        public void DepositsWhenAmountIsCorrect()
+        {
+            var account = new Account("1", _testBalance);
+
+            account.Deposit(_testAmountCorrect);
+
+            Assert.True(_testBalance < account.Balance);
+        }
+    }
+}
