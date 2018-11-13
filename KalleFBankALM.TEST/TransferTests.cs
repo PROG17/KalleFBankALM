0dc840e7bcff92fa15413bc9d97c6091abf2e12c
Jens <jens.hejneskog@yh.nackademin.se>
2018-11-13 20:04:17 +0100

    Added transfer between two accounts in new view. Added unit test for new method.

---
+++ KalleFBankALM.TEST/TransferTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace KalleFBankALM.TEST
+{
+    public class TransferTests
+    {
+        private string _fromAccountNumber = "54321";
+        private string _toAcccountNumber = "345123";
+        private decimal _transferAmount = 50m;
+        private BankRepository _bankRepo = new BankRepository();
+
+        [Fact]
+        public void TransferWithCorrectAccountNumbersAndAmount()
+        {
+            var fAccountBalance = _bankRepo.GetAccountById(_fromAccountNumber).Balance;
+            var tAccountBalance = _bankRepo.GetAccountById(_toAcccountNumber).Balance;
+            
+            var model = new ViewModels.TransferViewModel
+            {
+                FromAccountNumber = _fromAccountNumber,
+                ToAccountNumber = _toAcccountNumber,
+                TransferAmount = _transferAmount
+            };
+
+            model = _bankRepo.Transfer(model);
+            
+            Assert.Equal((model.FromAccount.Balance + _transferAmount), fAccountBalance);
+            Assert.Equal((model.ToAccount.Balance - _transferAmount), tAccountBalance);
+        }
+
+        [Fact]
+        public void TransferWithUnvalidAmount()
+        {
+            var model = new ViewModels.TransferViewModel
+            {
+                FromAccountNumber = _fromAccountNumber,
+                ToAccountNumber = _toAcccountNumber,
+                TransferAmount = 1000m
+            };
+
+            model = _bankRepo.Transfer(model);
+
+            Assert.True(model.FromAccount == null);
+            Assert.True(model.ToAccount == null);
+        }
+
+        [Fact]
+        public void TransferWithUnvalidAccountNumber()
+        {
+            var model = new ViewModels.TransferViewModel
+            {
+                FromAccountNumber = _fromAccountNumber,
+                ToAccountNumber = "112233",
+                TransferAmount = _transferAmount
+            };
+
+            model = _bankRepo.Transfer(model);
+
+            Assert.True(model.FromAccount == null);
+            Assert.True(model.ToAccount == null);
+        }
+    }
+}
---
+++ KalleFBankALM/ViewModels/TransferViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KalleFBankALM.Models;
+
+namespace KalleFBankALM.ViewModels
+{
+    public class TransferViewModel
+    {
+        public string FromAccountNumber { get; set; }
+        public string ToAccountNumber { get; set; }
+        public decimal TransferAmount { get; set; }
+        public Account FromAccount { get; set; }
+        public Account ToAccount { get; set; }
+        public string Message { get; set; }
+    }
+}
